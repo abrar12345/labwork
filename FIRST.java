bfe698d0a3625a459d793f24a4d6ec3e59553bd7
abrar12345 <86601116+abrar12345@users.noreply.github.com>
2021-07-14 14:08:24 -0700

    Add files via upload

---
+++ FIRST.java
@@ -0,0 +1,15 @@
+
+public class FIRST {
+public static void main(String[] args) {
+	
+
+	System.out.println("enter two number");
+	int first=100;
+	int second=200;
+	System.out.println(first+" "+second);
+	//add two number
+	int sum=first+second;
+System.out.print("the sum: "+sum);
+
+  } 
+}
