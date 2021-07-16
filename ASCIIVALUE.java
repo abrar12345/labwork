5339be7aecd0ef1d68e4acc349f9989cc10f3de5
abrar12345 <86601116+abrar12345@users.noreply.github.com>
2021-07-16 02:59:33 -0700

    Add files via upload

---
+++ ASCIIVALUE.java
@@ -0,0 +1,10 @@
+
+public class ASCIIVALUE {
+public static void main(String[] args) {
+	char ch ='$';
+	int ascii= ch;// you can also cast char into int
+	int castAscii=(int)ch;
+	System.out.println("the ascii value of "+ch+"is:"+ascii);
+	System.out.println("the ascii  value of "+ch+"is:"+castAscii);	
+}
+}
