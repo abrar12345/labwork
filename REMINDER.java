ba65ca7590bc79048a1df4758b3b64633c15eee9
abrar12345 <86601116+abrar12345@users.noreply.github.com>
2021-07-16 03:00:58 -0700

    Add files via upload

---
+++ REMINDER.java
@@ -0,0 +1,10 @@
+
+public class REMINDER {
+public static void main(String[] args) {
+	int divident =25,divisor=4;
+	int quotient =divident/divisor;
+	int reminder= divident%divisor;
+	System.out.println("Quotient="+quotient);
+	System.out.println("Reminder="+reminder);
+}
+}
