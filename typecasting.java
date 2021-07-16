2cf378afdcac899ec615b09b6e9615a4706fac6d
abrar12345 <86601116+abrar12345@users.noreply.github.com>
2021-07-16 02:58:19 -0700

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,19 @@
+
+public class typecasting {
+public static void main(String[] args) {
+	//double i=20;//int size is 32 bit or 4 byte
+	//int b=(int)i; // double size is 64 bit or  8 byte
+	//System.out.println(b);
+	byte b=10;//byte size is 8bit or 1 byte
+	int i=b;
+	//byte i=c;// will not accpect
+ byte d=(byte)i;
+ byte x=2;
+ byte y=1;
+byte sum =x*y;
+}
+
+
+}
+
+
