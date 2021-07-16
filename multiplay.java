34bc3488e79da494acb0a677b2ea7dc29faee42e
abrar12345 <86601116+abrar12345@users.noreply.github.com>
2021-07-16 02:59:07 -0700

    Add files via upload

---
+++ multiplay.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class multiplay {
+public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+    System.out.println("enter value for i...");
+int i =scan.nextInt();
+System.out.println("enter value for j....");
+int j= scan.nextInt();
+int mul=i*j;
+System.out.println("the  multiplication  of i and j is ...:"+mul);
+}
+}
